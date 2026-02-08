dd6aab3c39556eca5c227db80ebede5dd72a17e6
abidiyoussef086 <abidiyoussef086@gmail.com>
2026-02-08 20:41:38 +0100

    Fix formatting issues in MonitorLoop.ps1

--- MonitorLoop.ps1
+++ MonitorLoop.ps1
@@ -1,9 +1,10 @@
 . "C:\ Path \To\Get-ProcessInfo.ps1"
 
  while ($true ) {
-$processes = Get-HighCPUProcesses-CPUThreshold 10
+$processes = Get-HighCPUProcesses -CPUThreshold 10
  Clear-Host
- Write-Host " Processus haute consommation - $(Get-Date):"
- $processes | Format-Table-AutoSize
+ Write-Host " Processus haute consommation -$(Get-Date):"
+ $processes | Format-Table -AutoSize
  Start-Sleep -Seconds 10
+
 }
