3064681ff500e524557d8bd3f4326e9764497b16
abidiyoussef086 <abidiyoussef086@gmail.com>
2026-02-08 20:43:08 +0100

    Fix formatting and spacing in MainScript.ps1

--- MainScript.ps1
+++ MainScript.ps1
@@ -3,11 +3,12 @@
 $cpuThreshold = Read-Host " Entrez le seuil CPU (%) "
  $processes = Get-HighCPUProcesses - CPUThreshold $cpuThreshold
 
- $processes | Format-Table-AutoSize
+ $processes | Format-Table -AutoSize
 
  $export = Read-Host "Exporter les r s u l t a t s en CSV ? (O/N)"
  if ( $export -eq "O") {
  $file = "C:\ Temp \ HighCPUProcesses .csv "
- $processes | Export-Csv $file-NoTypeInformation
+ $processes | Export-Csv $file -NoTypeInformation
  Write-Host " R s u l t a t s s a u v e g a r d s dans $file "
  }
+
