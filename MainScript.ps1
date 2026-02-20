5596fdecb8f2eb67e71e434384df6fd2fa158275
abidi youssef <abidiyoussef086@gmail.com>
2026-02-20 10:56:42 +0100

    Add files via upload

--- MainScript.ps1
+++ MainScript.ps1
@@ -7,11 +7,12 @@ $processes | Format-Table -AutoSize
 
 $export = Read-Host "Exporter les resultats en CSV ? (O/N)"
 if ($export -eq "O") {
-    $dir = "C:\Temp"
+    $dir = Join-Path -Path ([Environment]::GetFolderPath("Desktop")) -ChildPath "powershell"
     if (-not (Test-Path -Path $dir)) {
         New-Item -Path $dir -ItemType Directory | Out-Null
     }
     $file = Join-Path -Path $dir -ChildPath "HighCPUProcesses.csv"
-    $processes | Export-Csv $file -NoTypeInformation
+    $processes | Export-Csv $file -NoTypeInformation -Force
     Write-Host "Resultats sauvegardes dans $file"
+    code $file
 }
